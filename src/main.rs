184304508411e20876660e0828d9b7c464c5fae9
dandk105 <d2cak5s.e43d@gmail.com>
2021-11-25 22:56:39 +0900

    add function of  checking ipv6addr type from arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::Ipv4Addr;
+use std::net::{IpAddr,Ipv4Addr};
 use pnet::packet::icmp::{IcmpPacket};
 use structopt::StructOpt;
 
@@ -13,11 +13,20 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
+    // 引数が不明な型だった場合に、パニックを起こす
+    let ipaddr = args.target_ip.parse().unwrap_or_else(|_| panic!("the inputed type dont accept in this app"));
+
+    // ipv6の型だった場合にエラーを起こす
+    if let true = Ipv4Addr::from_str(ipaddr).is_ipv6() {
+        panic!("Sorry this application have not accept ipv6addr type yet")
+    }
+
     if let true = args.debug {
-        let target_name: Ipv4Addr = args.target_ip.parse().unwrap();
+        let target_name: IpAddr = ipaddr;
         println!("Target_Name: {:?}",target_name)
     } else {
-        let demo: Ipv4Addr = args.target_ip.parse().unwrap();
-        let icmp_packet = IcmpPacket::new(demo);
+        // どこで判断して、IpAddr型だと判断しているんだろうか？
+        let ipv4addr: &[u8]  = ipaddr.as_bytes();
+        let icmp_packet = IcmpPacket::new(ipv4addr);
     }
 }
