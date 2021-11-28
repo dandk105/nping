02e6fef3a628f5962c64457026e67347fe3e98f1
dandk105 <d2cak5s.e43d@gmail.com>
2021-11-28 18:19:03 +0900

    add custom error and fuction of checking version of ip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use std::net::{IpAddr,Ipv4Addr};
+use std::fmt;
+
 use pnet::packet::icmp::{IcmpPacket};
 use structopt::StructOpt;
 
+
+#[derive(Debug)]
+struct UnusedIpv6addr;
+
+impl fmt::Display for UnusedIpv6addr {
+    fn fmt(&self, f: &mut fmt::Formatter ) -> fmt::Result {
+        match *self {
+            UnusedIpv6addr => f.write_str("This application not have allowed ipv6addr type yet")
+        }
+    }
+}
 
 #[derive(Debug, StructOpt)]
 struct Cli {
@@ -11,22 +24,37 @@ struct Cli {
     target_ip: String
 }
 
+fn check_ipv4(target: String) -> Result<Ipv4Addr,UnusedIpv6addr> {
+    // もしtargetが、ipv4アドレス型に一致するなら、
+    // Ipv4addrを返却する
+    // Ipv6アドレス型に一致するなら、パニックを起こす
+    use std::str::FromStr;
+    // 1行下では、エラーが起きると、おそらく、UnusedIpv6addrに吸収される
+    // だけど、1行下で発生される、エラーは、そこに吸収されたくないので、別の実装が必要と思われる
+    let ip: IpAddr = IpAddr::from_str(&target).unwrap();
+    if let true = ip.is_ipv6() {
+        panic!("Sorry this application have not accept Ipv6addr yet")
+    } else {
+        let ipv4addr = Ipv4Addr::from_str(&target).unwrap();
+        Ok(ipv4addr)
+    }
+}
+
 fn main() {
     let args = Cli::from_args();
     // 引数が不明な型だった場合に、パニックを起こす
-    let ipaddr = args.target_ip.parse().unwrap_or_else(|_| panic!("the inputed type dont accept in this app"));
+    // trget_ipの型が示すのが何かわかっていないから混乱しているのでは？
+    // struct_optで示されているのは、target_ipの型
+    let target: String = args.target_ip.parse().unwrap_or_else(|_| panic!("the inputed type dont accept in this app"));
 
     // ipv6の型だった場合にエラーを起こす
-    if let true = Ipv4Addr::from_str(ipaddr).is_ipv6() {
-        panic!("Sorry this application have not accept ipv6addr type yet")
-    }
+    // ipv4だとresult返す
+    let ipv4addr = check_ipv4(target).unwrap();
 
     if let true = args.debug {
-        let target_name: IpAddr = ipaddr;
-        println!("Target_Name: {:?}",target_name)
+        println!("Target_Name: {:?}", ipv4addr)
     } else {
-        // どこで判断して、IpAddr型だと判断しているんだろうか？
-        let ipv4addr: &[u8]  = ipaddr.as_bytes();
+        let ipv4addr: &[u8]  = ipv4addr;
         let icmp_packet = IcmpPacket::new(ipv4addr);
     }
 }
