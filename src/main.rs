5ff36ed29f2694c4755ae6c85dc310b76fb8d7a4
dandk105 <d2cak5s.e43d@gmail.com>
2021-11-28 20:18:03 +0900

    implimationed showing a error message when use ipv6addr in arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::{IpAddr,Ipv4Addr};
+use std::net::{IpAddr};
 use std::fmt;
 
 use pnet::packet::icmp::{IcmpPacket};
@@ -11,7 +11,7 @@ struct UnusedIpv6addr;
 impl fmt::Display for UnusedIpv6addr {
     fn fmt(&self, f: &mut fmt::Formatter ) -> fmt::Result {
         match *self {
-            UnusedIpv6addr => f.write_str("This application not have allowed ipv6addr type yet")
+            UnusedIpv6addr => f.write_str("Sorry This application not have allowed ipv6addr type yet \n")
         }
     }
 }
@@ -24,37 +24,37 @@ struct Cli {
     target_ip: String
 }
 
-fn check_ipv4(target: String) -> Result<Ipv4Addr,UnusedIpv6addr> {
+fn check_ipv4(a: &String) -> Result<bool,UnusedIpv6addr> {
     // もしtargetが、ipv4アドレス型に一致するなら、
-    // Ipv4addrを返却する
+    // Trueを返却する
     // Ipv6アドレス型に一致するなら、パニックを起こす
     use std::str::FromStr;
     // 1行下では、エラーが起きると、おそらく、UnusedIpv6addrに吸収される
     // だけど、1行下で発生される、エラーは、そこに吸収されたくないので、別の実装が必要と思われる
-    let ip: IpAddr = IpAddr::from_str(&target).unwrap();
-    if let true = ip.is_ipv6() {
-        panic!("Sorry this application have not accept Ipv6addr yet")
+    let ip: IpAddr = IpAddr::from_str(&a).unwrap_or_else(|_| panic!());
+    if let true = ip.is_ipv4() {
+        Ok(true)
     } else {
-        let ipv4addr = Ipv4Addr::from_str(&target).unwrap();
-        Ok(ipv4addr)
+        Err(UnusedIpv6addr)
     }
 }
 
 fn main() {
     let args = Cli::from_args();
-    // 引数が不明な型だった場合に、パニックを起こす
-    // trget_ipの型が示すのが何かわかっていないから混乱しているのでは？
-    // struct_optで示されているのは、target_ipの型
-    let target: String = args.target_ip.parse().unwrap_or_else(|_| panic!("the inputed type dont accept in this app"));
+    let target: String = args.target_ip.parse().unwrap();
 
-    // ipv6の型だった場合にエラーを起こす
-    // ipv4だとresult返す
-    let ipv4addr = check_ipv4(target).unwrap();
+    // グローバル変数として、空の文字列を定義して、check_ipv4関数が正の場合
+    // グローバル変数に、targetの参照を代入という手法をとっているが、すごいダサい。
+    let mut ipv4addr = "";
+    match check_ipv4(&target) {
+        Ok(_) => ipv4addr = &target,
+        Err(err) => print!("{}",err)
+    }
 
     if let true = args.debug {
         println!("Target_Name: {:?}", ipv4addr)
     } else {
-        let ipv4addr: &[u8]  = ipv4addr;
+        let ipv4addr: &[u8]  = ipv4addr.as_ref();
         let icmp_packet = IcmpPacket::new(ipv4addr);
     }
 }
